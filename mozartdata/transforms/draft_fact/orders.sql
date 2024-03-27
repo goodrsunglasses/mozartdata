2db463d7423e7d5b5f44ae2221774c30f9a40edd
Kaden-SL <businessintel@goodr.com>
2024-03-27 15:27:44 -0700

    Update orders.sql

--- mozartdata/transforms/draft_fact/orders.sql
+++ mozartdata/transforms/draft_fact/orders.sql
@@ -27,7 +27,7 @@ WITH
       LEFT OUTER JOIN fact.shopify_order_line shopify_line ON shopify_line.order_id_shopify = orders.order_id_shopify
   ),
   
-  fulfillment_info AS (--Grab fulfillment order information needs adjustment in dim.orders and dim.fulfillment, adding this as a test merge
+  fulfillment_info AS (--Grab fulfillment order information needs adjustment in dim.orders and dim.fulfillment, adding this as a test merge, test merge two
     SELECT
       orders.order_id_edw,
       order_created_date_pst,
