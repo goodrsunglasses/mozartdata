9eb9a0fae0fa16cf123f304c70352d271f889eaf
Josha Russell <russell.josha@gmail.com>
2024-04-22 11:18:45 -0700

    added shipping price to shopify

--- mozartdata/transforms/fact/shopify_order_line.sql
+++ mozartdata/transforms/fact/shopify_order_line.sql
@@ -1,11 +1,13 @@
+CREATE OR REPLACE TABLE fact.shopify_order_line COPY GRANTS AS
 SELECT DISTINCT
   d2c_shop.name order_id_edw,
   d2c_shop.id order_id_shopify,
   'Goodr.com' AS store,
   d2c_shop.email,
-  d2c_shop.subtotal_price as amount_booked,
-  d2c_shop.total_price as amount_sold,
+  d2c_shop.total_line_items_price as amount_booked,
+  ship.price as shipping_sold,
   d2c_shop.total_tax as tax_sold,
+  d2c_shop.total_price as amount_sold,
   d2c_shop.total_discounts as amount_discount,
   d2c_shop.created_at as order_created_timestamp,
   DATE(d2c_shop.created_at) as order_created_date,
@@ -21,20 +23,22 @@ SELECT DISTINCT
   d2c_shop.checkout_id as checkout_id_shopify,
   SUM(quantity) over (
     PARTITION BY
-      order_id
+      line.order_id
   ) quantity_sold
 FROM
   shopify."ORDER" d2c_shop
   LEFT OUTER JOIN shopify.order_line line ON line.order_id = d2c_shop.id
+  LEFT OUTER JOIN shopify.order_shipping_line ship ON ship.order_id = d2c_shop.id
 UNION ALL
 SELECT DISTINCT
   b2b_shop.name order_id_edw,
   b2b_shop.id order_id_shopify,
   'Specialty' AS store,
   b2b_shop.email,
-  b2b_shop.subtotal_price as amount_booked,
-  b2b_shop.total_price as amount_sold,
+  b2b_shop.total_line_items_price as amount_booked,
+  ship.price as shipping_sold,
   b2b_shop.total_tax as tax_sold,
+  b2b_shop.total_price as amount_sold,
   b2b_shop.total_discounts as amount_discount,
   b2b_shop.created_at as order_created_timestamp,
   DATE(b2b_shop.created_at) as order_created_date,
@@ -50,20 +54,22 @@ SELECT DISTINCT
   b2b_shop.checkout_id as checkout_id_shopify,
   SUM(quantity) over (
     PARTITION BY
-      order_id
+      line.order_id
   ) quantity_sold
 FROM
   specialty_shopify."ORDER" b2b_shop
   LEFT OUTER JOIN specialty_shopify.order_line line ON line.order_id = b2b_shop.id
+  LEFT OUTER JOIN shopify.order_shipping_line ship ON ship.order_id = b2b_shop.id
 UNION ALL
 SELECT DISTINCT
   d2c_can_shop.name order_id_edw,
   d2c_can_shop.id order_id_shopify,
   'Canada D2C' AS store,
   d2c_can_shop.email,
-  d2c_can_shop.subtotal_price as amount_booked,
-  d2c_can_shop.total_price as amount_sold,
+  d2c_can_shop.total_line_items_price as amount_booked,
+  ship.price as shipping_sold,
   d2c_can_shop.total_tax as tax_sold,
+  d2c_can_shop.total_price as amount_sold,
   d2c_can_shop.total_discounts as amount_discount,
   d2c_can_shop.created_at as order_created_timestamp,
   DATE(d2c_can_shop.created_at) as order_created_date,
@@ -79,20 +85,22 @@ SELECT DISTINCT
   d2c_can_shop.checkout_id as checkout_id_shopify,
   SUM(quantity) over (
     PARTITION BY
-      order_id
+      line.order_id
   ) quantity_sold
 FROM
   goodr_canada_shopify."ORDER" d2c_can_shop
   LEFT OUTER JOIN specialty_shopify.order_line line ON line.order_id = d2c_can_shop.id
+  LEFT OUTER JOIN shopify.order_shipping_line ship ON ship.order_id = d2c_can_shop.id
 UNION ALL
 SELECT DISTINCT
   b2b_can_shop.name order_id_edw,
   b2b_can_shop.id order_id_shopify,
   'Specialty Canada' AS store,
   b2b_can_shop.email,
-  b2b_can_shop.subtotal_price as amount_booked,
-  b2b_can_shop.total_price as amount_sold,
+  b2b_can_shop.total_line_items_price as amount_booked,
+  ship.price as shipping_sold,
   b2b_can_shop.total_tax as tax_sold,
+  b2b_can_shop.total_price as amount_sold,
   b2b_can_shop.total_discounts as amount_discount,
   b2b_can_shop.created_at as order_created_timestamp,
   DATE(b2b_can_shop.created_at) as order_created_date,
@@ -108,20 +116,22 @@ SELECT DISTINCT
   b2b_can_shop.checkout_id as checkout_id_shopify,
   SUM(quantity) over (
     PARTITION BY
-      order_id
+      line.order_id
   ) quantity_sold
 FROM
   sellgoodr_canada_shopify."ORDER" b2b_can_shop
   LEFT OUTER JOIN specialty_shopify.order_line line ON line.order_id = b2b_can_shop.id
+  LEFT OUTER JOIN shopify.order_shipping_line ship ON ship.order_id = b2b_can_shop.id
 UNION ALL
 SELECT DISTINCT
   goodrwill.name order_id_edw,
   goodrwill.id order_id_shopify,
   'Goodrwill' AS store,
   goodrwill.email,
-  goodrwill.subtotal_price as amount_booked,
-  goodrwill.total_price as amount_sold,
+  goodrwill.total_line_items_price as amount_booked,
+  ship.price as shipping_sold,
   goodrwill.total_tax as tax_sold,
+  goodrwill.total_price as amount_sold,
   goodrwill.total_discounts as amount_discount,
   goodrwill.created_at as order_created_timestamp,
   DATE(goodrwill.created_at) as order_created_date,
@@ -137,8 +147,9 @@ SELECT DISTINCT
   goodrwill.checkout_id as checkout_id_shopify,
   SUM(quantity) over (
     PARTITION BY
-      order_id
+      line.order_id
   ) quantity_sold
 FROM
   goodrwill_shopify."ORDER" goodrwill
   LEFT OUTER JOIN specialty_shopify.order_line line ON line.order_id = goodrwill.id
+  LEFT OUTER JOIN shopify.order_shipping_line ship ON ship.order_id = goodrwill.id
