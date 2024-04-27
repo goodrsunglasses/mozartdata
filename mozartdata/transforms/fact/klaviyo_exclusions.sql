87a019124a22bf281de0e548afcf2e1ba322ed60
Josha Russell <russell.josha@gmail.com>
2024-04-27 08:20:18 -0700

    klaviyo_exclusions documentation and code cleanup

--- mozartdata/transforms/fact/klaviyo_exclusions.sql
+++ mozartdata/transforms/fact/klaviyo_exclusions.sql
@@ -1,6 +1,15 @@
-SELECT 
+/*
+Purpose: This table contains meta data about Klaviyo Exclussions. This data comes from an API connection
+set up through the vendor Portable which directly feeds the data into our Snowflake db.
+Transforms:all dates are natively in UTC, so I converted them to LA time.
+About this data: This shows when a user is added to an exclusion list and why. Generally they are marking our emails
+as spam or unsubscribing.
+*/
+SELECT
   e.timestamp as exclusion_timestamp
+, convert_timezone('UTC', 'America/Los_Angeles', e.timestamp) as exclusion_timestamp_pst
 , date(e.timestamp) as exclusion_date
+, date(convert_timezone('UTC', 'America/Los_Angeles', e.timestamp)) as exclusion_date_pst
 , e.email
 , e.object as type
 , e.reason
