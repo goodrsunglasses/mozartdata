3a2c8813251787a34a85db7df4db9a9ab2b63606
Josha Russell <russell.josha@gmail.com>
2024-05-05 22:37:21 -0700

    created gl_balances fact.

--- mozartdata/transforms/fact/gl_balances.sql
+++ mozartdata/transforms/fact/gl_balances.sql
@@ -1,6 +1,36 @@
+/*
+ Purpose: this table creates monthly balances for balance sheet accounts.
+Transforms: In this query we combine all balance sheet accounts with all posting periods from december 2021 to present
+ Balance sheet accounts are accounts 1000-3999
 
-
-with all_balances as
+ */
+--This CTE creates every combination of balance sheet account and posting periods beginning in December 2021
+with account_periods as
+  (
+    select
+      ap.posting_period
+    , ap.period_start_date
+    , ga_sub.account_number
+    , ga_sub.account_id_edw
+    from
+      dim.accounting_period ap
+    inner join
+      (select
+         ga.account_number
+       , ga.account_id_edw
+       from
+         dim.gl_account ga
+       where
+         ga.account_number between 1000 and 3999
+       and ga.active_flag = true) ga_sub
+      on 1=1
+    where
+      ap.active_flag = true
+      and ap.is_posting_flag = true
+      and ap.period_start_date >= '2021-12-01'
+  )
+-- This CTE captures the starting balances from google_sheets.balance_dec_21 with all GL activity for balance sheet accounts
+, gl_activity as
   (
 SELECT
   h.posting_period
@@ -30,15 +60,31 @@ GROUP BY
 , gt.account_number
 , gt.account_id_ns
 , gt.account_id_edw
-)
+), all_activity as
+(
 select
-  ab.posting_period
-, ab.account_id_edw
-, ab.account_number
-, ab.balance as current_period_amount
-, sum(ab.balance) over (partition by ab.account_number order by ab.posting_date) as ending_balance
+  ap.posting_period
+, ap.period_start_date
+, ap.account_number
+, ap.account_id_edw
+, coalesce(ga.balance,0) as balance
+from
+  account_periods ap
+left join
+gl_activity ga
+on ap.posting_period = ga.posting_period
+  and ap.account_id_edw = ga.account_id_edw
+)
+
+select
+  aa.posting_period
+, aa.period_start_date
+, aa.account_id_edw
+, aa.account_number
+, round(aa.balance,2) as current_period_amount
+, sum(aa.balance) over (partition by aa.account_number order by aa.period_start_date) as ending_balance
 FROM
-  all_balances ab
+  all_activity aa
 order by
-  ab.account_number
-, ab.posting_date
+  aa.account_number
+, aa.period_start_date
