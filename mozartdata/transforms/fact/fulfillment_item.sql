e7e78ebe3043c1157a6137ff1b166e7a3158e87d
Kaden-SL <ksedmakl@ucsc.edu>
2024-05-16 15:16:19 -0700

    Final item based patch for fact.fulfillment item detail, 0 TOTAL NULL SKU'S BAYBEE

--- mozartdata/transforms/fact/fulfillment_item.sql
+++ mozartdata/transforms/fact/fulfillment_item.sql
@@ -1,6 +1,23 @@
-SELECT DISTINCT --added a distinct to handle NS information potentially being incorrect
-                fulfillment_id_edw,
-                order_id_edw,
-                product_id_edw,
-                quantity
-FROM fact.fulfillment_item_detail
+WITH ss_qty AS (SELECT fulfillment_id_edw,
+					   order_id_edw,
+					   sku,
+					   ITEM_ID,
+					   quantity
+				FROM fact.fulfillment_item_detail
+				WHERE source = 'Shipstation'),
+	 stord_qty AS (SELECT fulfillment_id_edw,
+						  order_id_edw,
+						  sku,
+						  ITEM_ID,
+						  quantity
+				   FROM fact.fulfillment_item_detail
+				   WHERE source = 'Stord'),
+	 netsuite_qty AS (SELECT fulfillment_id_edw, order_id_edw, sku, ITEM_ID, quantity
+					  FROM fact.fulfillment_item_detail
+					  WHERE source = 'Netsuite')
+SELECT fulfillment_id_edw,
+	   order_id_edw
+FROM fact.FULFILLMENT_ITEM_DETAIL
+
+
+WHERE ORDER_ID_EDW LIKE '%CS-LST-SD-G2501679%'
