6319f31e96b147d2c216ed3dca7809508c05e402
Kaden-SL <businessintel@goodr.com>
2024-05-21 22:42:07 -0700

    Pushed everything except the orders changes cuz gabby needed to wait on that I think

--- mozartdata/transforms/fact/fulfillment.sql
+++ mozartdata/transforms/fact/fulfillment.sql
@@ -1,5 +1,6 @@
-WITH stord_info AS (SELECT FULFILLMENT_ID_EDW,
+WITH stord_line AS (SELECT FULFILLMENT_ID_EDW,
 						   ORDER_ID_EDW,
+						   SHIPMENT_ID,
 						   carrier,
 						   carrier_service,
 						   shipdate,
@@ -9,20 +10,45 @@ WITH stord_info AS (SELECT FULFILLMENT_ID_EDW,
 						   CASE
 							   WHEN warehouse_location LIKE 'ATL%' THEN 'ATL'
 							   WHEN warehouse_location LIKE 'LAS%' THEN 'LAS'
-							   ELSE NULL END AS warehouse_location
+							   ELSE Null END AS warehouse_location
 					FROM fact.FULFILLMENT_LINE
 					WHERE source = 'Stord'),
-	ss_info as( select FULFILLMENT_ID_EDW,
-						   ORDER_ID_EDW,
-						   carrier,
-						   carrier_service,
-						   shipdate,
-						   country,
-						   state,
-						   city
-						   FROM fact.FULFILLMENT_LINE
-					WHERE source = 'Shipstation'),
-ns_info as ( select *
-						   FROM fact.FULFILLMENT_LINE
-					WHERE source = 'Netsuite')
-select fulfill.FULFILLMENT_ID_EDW,fulfill.ORDER_ID_EDW from dim.FULFILLMENT fulfill
+	 ss_line AS (SELECT FULFILLMENT_ID_EDW,
+						ORDER_ID_EDW,
+						SHIPMENT_ID,
+						carrier,
+						carrier_service,
+						shipdate,
+						country,
+						state,
+						city
+				 FROM fact.FULFILLMENT_LINE
+				 WHERE source = 'Shipstation'),
+	 ns_info AS (SELECT *
+				 FROM fact.FULFILLMENT_LINE
+				 WHERE source = 'Netsuite'),
+	 aggregates AS (SELECT FULFILLMENT_ID_EDW,
+						   SUM(QUANTITY_NS)    AS total_QUANTITY_NS,
+						   SUM(QUANTITY_STORD) AS total_QUANTITY_STORD,
+						   SUM(QUANTITY_SS)    AS total_QUANTITY_SS
+					FROM fact.FULFILLMENT_ITEM
+					GROUP BY FULFILLMENT_ID_EDW)
+SELECT fulfill.FULFILLMENT_ID_EDW,
+	   fulfill.ORDER_ID_EDW,
+	   COALESCE(stord.carrier, ss.CARRIER)                 AS carrier, --we can use coalesce logic because a given fulfillment_id_edw isn't present in both stord and shipstation (I checked to be sure)
+	   COALESCE(stord.carrier_service, ss.carrier_service) AS carrier_service,
+	   COALESCE(stord.shipdate, ss.shipdate)               AS shipdate,
+	   COALESCE(stord.country, ss.country)                 AS country,
+	   COALESCE(stord.state, ss.state)                     AS state,
+	   COALESCE(stord.city, ss.city)                       AS city,
+	   coalesce(stord.warehouse_location,'Lagoon')as warehouse_location,--As of right now it only has logic for either stord ATL or LAS, if thats not the case then it just fills it in as Lagoon, as the asumption is everything else had to go out of there
+	   aggregates.total_QUANTITY_NS,
+	   aggregates.total_QUANTITY_STORD,
+	   aggregates.total_QUANTITY_SS
+
+FROM dim.FULFILLMENT fulfill
+		 LEFT OUTER JOIN stord_line stord ON (stord.FULFILLMENT_ID_EDW = fulfill.FULFILLMENT_ID_EDW AND
+											  stord.SHIPMENT_ID = fulfill.SOURCE_SYSTEM_ID)
+		 LEFT OUTER JOIN ss_line ss ON (ss.FULFILLMENT_ID_EDW = fulfill.FULFILLMENT_ID_EDW AND
+										ss.SHIPMENT_ID = fulfill.SOURCE_SYSTEM_ID)
+		 LEFT OUTER JOIN aggregates ON aggregates.FULFILLMENT_ID_EDW = fulfill.FULFILLMENT_ID_EDW
--- mozartdata/transforms/fact/fulfillment_item_detail.sql
+++ mozartdata/transforms/fact/fulfillment_item_detail.sql
@@ -6,10 +6,11 @@ SELECT fulfillment_id_edw,
 	   shipdate,
 -- 	   shipmentcost                            AS shipment_cost, Gabby said no
 	   voided,
-	   null as warehouse_location,
+	   NULL                                    AS warehouse_location,
 	   shipto:STATE::STRING                    AS state,
 	   shipto:COUNTRY::STRING                  AS country,
 	   shipto:CITY::STRING                     AS city,
+	   shipto:POSTALCODE::STRING               AS postal_code,
 	   TO_CHAR(items.shipmentid)               AS shipment_id,
 	   flattened_items.value:PRODUCTID::STRING AS item_id,
 	   product.sku,
@@ -25,29 +26,30 @@ FROM dim.fulfillment fulfill
 WHERE source_system = 'Shipstation'
 --Stord
 UNION ALL
-SELECT distinct fulfillment_id_edw,
-	   fulfill.ORDER_ID_EDW,
-	   'Stord'                                                                AS source,
-	   stord.carrier_name,
-	   stord.carrier_service_method,
-	   stord.shipped_at,
+SELECT DISTINCT fulfillment_id_edw,
+				fulfill.ORDER_ID_EDW,
+				'Stord'                                                                AS source,
+				stord.carrier_name,
+				stord.carrier_service_method,
+				stord.shipped_at,
 -- 	   NULL                                                                   AS shipmentcost,
-	   is_canceled,
-	   sla_lines.value:FACILITY_ACTIVITY:FACILITY_ALIAS as facility_alias,
-	   orders.destination_address:NORMALIZED_COUNTRY_SUBDIVISION_CODE::STRING AS state,
-	   orders.destination_address:NORMALIZED_COUNTRY_CODE::STRING             AS country,
-	   orders.destination_address:NORMALIZED_LOCALITY::STRING                 AS city,
-	   shipment_confirmation_id                                               AS shipment_id,
-	   flattened_items.value:ITEM_ID::STRING                                  AS item_id,
-	   product.sku,
-	   stordprod.name,
-	   flattened_items.value:QUANTITY::INTEGER                                AS quantity
+				is_canceled,
+				sla_lines.value:FACILITY_ACTIVITY:FACILITY_ALIAS                       AS facility_alias,
+				orders.destination_address:NORMALIZED_COUNTRY_SUBDIVISION_CODE::STRING AS state,
+				orders.destination_address:NORMALIZED_COUNTRY_CODE::STRING             AS country,
+				orders.destination_address:NORMALIZED_LOCALITY::STRING                 AS city,
+				orders.destination_address:NORMALIZED_POSTAL_CODE::STRING              AS postal_code,
+				shipment_confirmation_id                                               AS shipment_id,
+				flattened_items.value:ITEM_ID::STRING                                  AS item_id,
+				product.sku,
+				stordprod.name,
+				flattened_items.value:QUANTITY::INTEGER                                AS quantity
 FROM dim.fulfillment fulfill
 		 LEFT OUTER JOIN stord.stord_shipment_confirmations_8589936822 stord
 						 ON stord.shipment_confirmation_id = fulfill.source_system_id
 		 LEFT OUTER JOIN stord.stord_sales_orders_8589936822 orders ON orders.order_id = stord.order_id
 		 CROSS JOIN LATERAL FLATTEN(INPUT => stord.SHIPMENT_CONFIRMATION_LINE_ITEMS) AS flattened_items
-	CROSS JOIN LATERAL FLATTEN(INPUT => orders.SLA_SALES_ORDER_LINES) AS sla_lines
+		 CROSS JOIN LATERAL FLATTEN(INPUT => orders.SLA_SALES_ORDER_LINES) AS sla_lines
 		 LEFT OUTER JOIN dim.product product ON product.item_id_stord = flattened_items.value:ITEM_ID::STRING
 		 LEFT OUTER JOIN stord.STORD_PRODUCTS_8589936822 stordprod
 						 ON stordprod.id = flattened_items.value:ITEM_ID::STRING --Joining here because I want the name of the product because sometimes it doesn't exist in NS
@@ -57,18 +59,19 @@ UNION ALL
 SELECT DISTINCT --adding just in case because NS joins can be funky and I don't want any duplicate lines becase one custom field has two values or something
 				fulfill.FULFILLMENT_ID_EDW,
 				fulfill.ORDER_ID_EDW,
-				'Netsuite'                    AS                                         source,
+				'Netsuite'                    AS source,
 				custbody_shipstation_carrier_code,
 				custbody_service_code,
-				TO_TIMESTAMP_NTZ(createddate) AS                                         shipped_at, --SADLY WE HAVE TO USE CREATEDDATE AS THE SHIPPING DATE AND JUST HOPE THAT IT WAS CREATED/SHIPPED THE SAME DAY BECAUSE NS DOESN'T STORE SHIPPEDDATE ANYWHERE
+				TO_TIMESTAMP_NTZ(createddate) AS shipped_at, --SADLY WE HAVE TO USE CREATEDDATE AS THE SHIPPING DATE AND JUST HOPE THAT IT WAS CREATED/SHIPPED THE SAME DAY BECAUSE NS DOESN'T STORE SHIPPEDDATE ANYWHERE
 -- 				NULL                          AS                                         shipmentcost,
-				NULL                          AS                                         is_cancelled,
+				NULL                          AS is_cancelled,
 				staged.location,
 				shipping.state,
 				shipping.country,
 				shipping.city,
-				to_char(staged.transaction_id_ns),
-				NULL                          AS                                         item_id,
+				shipping.zip                  AS postal_code,
+				TO_CHAR(staged.transaction_id_ns),
+				NULL                          AS item_id,
 				product.sku,
 				PLAIN_NAME,
 				total_quantity
