89080a456da5f5513cb8346d9b0c7a203bb04323
goodrbiflock <126719614+goodrbiflock@users.noreply.github.com>
2024-05-29 16:50:06 -0700

    Update mozartdata/transforms/staging/shopify_products.sql
    Sounds solid
    
    Co-authored-by: goodrsamwise <165083433+goodrsamwise@users.noreply.github.com>

--- mozartdata/transforms/staging/shopify_products.sql
+++ mozartdata/transforms/staging/shopify_products.sql
@@ -33,20 +33,21 @@ SELECT PRODUCT_ID,
 FROM SPECIALTY_SHOPIFY.PRODUCT_VARIANT variant
 		 LEFT OUTER JOIN SPECIALTY_SHOPIFY.PRODUCT prod ON prod.id = variant.PRODUCT_ID
 UNION ALL
-SELECT PRODUCT_ID,
+SELECT 
+           variant.PRODUCT_ID,
 	   prod.title,
-	   product_type,
+	   prod.product_type,
 	   variant.id    AS variant_id,
-	   status,
-	   INVENTORY_ITEM_ID,
+	   prod.status,
+	   variant.INVENTORY_ITEM_ID,
 	   variant.title AS variant_title,
-	   price,
-	   sku,
-	   barcode,
-	   grams,
-	   weight,
-	   weight_unit,
-	   option_1
+	   variant.price,
+	   variant.sku,
+	   variant.barcode,
+	   variant.grams,
+	   variant.weight,
+	   variant.weight_unit,
+	   variant.option_1
 FROM GOODRWILL_SHOPIFY.PRODUCT_VARIANT variant
 		 LEFT OUTER JOIN GOODRWILL_SHOPIFY.PRODUCT prod ON prod.id = variant.PRODUCT_ID
 UNION ALL
