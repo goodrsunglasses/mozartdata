45ff915717f7401f1ead1739b45b98bf15009306
Josha Russell <russell.josha@gmail.com>
2024-06-10 13:51:57 -0700

    added campaign and media strategy

--- mozartdata/transforms/dim/tiktok_campaigns.sql
+++ mozartdata/transforms/dim/tiktok_campaigns.sql
@@ -7,6 +7,18 @@ select distinct
   when ch.CAMPAIGN_NAME like '%BOF%' then 'BOF'
   else 'OTHER'
   end as funnel_stage
+, case
+  when ch.CAMPAIGN_NAME like '%TOF%' then 'Reach'
+  when ch.CAMPAIGN_NAME like '%MOF%' then 'Traffic, Engagement'
+  when ch.CAMPAIGN_NAME like '%BOF%' then 'Sales, Conversions'
+  else 'Other'
+  end as campaign_strategy
+, case
+  when ch.CAMPAIGN_NAME like '%TOF%' then 'Awareness'
+  when ch.CAMPAIGN_NAME like '%MOF%' then 'Awareness'
+  when ch.CAMPAIGN_NAME like '%BOF%' then 'Performance'
+  else 'Other'
+  end as media_strategy
 , ch.CAMPAIGN_TYPE
 , ch.BUDGET_MODE
 , ch.OPERATION_STATUS
