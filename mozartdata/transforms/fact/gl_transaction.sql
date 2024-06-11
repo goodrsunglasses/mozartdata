f22f02e0e8c88524af136280dd71013ddaf3479c
Josha Russell <russell.josha@gmail.com>
2024-06-11 08:41:27 -0700

    removed duplicate record type

--- mozartdata/transforms/fact/gl_transaction.sql
+++ mozartdata/transforms/fact/gl_transaction.sql
@@ -28,7 +28,6 @@ use createdate converted instead of trandate
     , tal."ACCOUNT" as account_id_ns
     , ga.account_number
     , channel.name as channel
-    , tran.recordtype AS record_type
     , tran.trandate as transaction_timestamp
     , date(tran.trandate) as transaction_date
     , CONVERT_TIMEZONE('America/Los_Angeles', tran.trandate) AS transaction_timestamp_pst
@@ -99,7 +98,6 @@ use createdate converted instead of trandate
     , tal."ACCOUNT"
     , ga.account_number
     , channel.name
-    , tran.recordtype
     , tran.trandate
     , pe.eventdate
     , ap.posting_period
