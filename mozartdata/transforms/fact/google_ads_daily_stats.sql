6311bd6841ded7c92c363a7a8b79ed53f1f14e6c
goodrsamwise <samnickolls@goodr.com>
2024-06-18 15:38:03 -0700

    Added a funnel_stage column for performance vs awareness data

--- mozartdata/transforms/fact/google_ads_daily_stats.sql
+++ mozartdata/transforms/fact/google_ads_daily_stats.sql
@@ -1,11 +1,19 @@
 select
     date
+  , case
+        when name like '%BOF%'
+            then 'Performance'
+        when name like '%TOF%' or name like '%MOF%'
+            then 'Awareness'
+        else name
+    end                        as funnel_stage
   , round(sum(revenue), 2)     as revenue
-  , round(sum(spend) , 2)      as spend
-  , sum(impressions) as impressions
-  , sum(clicks)      as clicks
+  , round(sum(spend), 2)       as spend
+  , sum(impressions)           as impressions
+  , sum(clicks)                as clicks
   , round(sum(conversions), 2) as conversions
 from
     fact.GOOGLE_ADS_CAMPAIGNS_DAILY_STATS
 group by
     date
+  , funnel_stage
