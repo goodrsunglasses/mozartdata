8e8dcc98d1afeeff154db15e528ec7ab986cb161
Yoshu22 <russell.josha@gmail.com>
2024-06-18 15:04:46 -0700

    updated sports, pop and logistics to use lookup tables

--- mozartdata/transforms/staging/netsuite_customers.sql
+++ mozartdata/transforms/staging/netsuite_customers.sql
@@ -32,9 +32,12 @@ SELECT cust.id                                                                 A
 	   cust.DATECREATED,
 	   cust.DEFAULTBILLINGADDRESS,
 	   cust.DEFAULTSHIPPINGADDRESS,
-	   cust.custentityam_primary_sport as primary_sport,
-     cust.custentityam_secondary_sport as secondary_sport,
-     cust.custentityam_tertiary_sport as tertiary_sport,
+	   cust.custentityam_primary_sport as primary_sport_id_ns,
+     pri.name as primary_sport,
+     cust.custentityam_secondary_sport as secondary_sport_id_ns,
+     sec.name as secondary_sport,
+     cust.custentityam_tertiary_sport as tertiary_sport_id_ns,
+     ter.name as tertiary_sport_id_ns,
      coalesce(parent_tier.id,tiers.id) as tier_id_ns,
      coalesce(parent_tier.name,tiers.name) as tier_ns,
      case when coalesce(parent_tier.name,tiers.name) = 'Named' then
@@ -43,8 +46,10 @@ SELECT cust.id                                                                 A
      cust.custentityam_doors as doors,
      cust.custentityam_buyer_name as buyer_name,
      cust.custentityam_buyer_email as buyer_email,
-     cust.custentityam_pop as pop,
-     cust.custentityam_logistics as logistics,
+     cust.custentityam_pop as pop_id_ns,
+     pop.name as pop,
+     cust.custentityam_logistics as logistics_id_ns,
+     log.name as logistics,
      cust.custentityam_city_1 as city_1,
      cust.custentityam_city_2 as city_2,
      cust.custentityam_city_3 as city_3,
@@ -61,5 +66,15 @@ FROM netsuite.customer cust
           ON cust.custentityam_tier = tiers.id
     LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_TIERS parent_tier
           ON parent.custentityam_tier = parent_tier.id
+    LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_TIERS pop
+          ON cust.custentityam_pop = pop.id
+    LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_LOGISTICS log
+          ON cust.custentityam_logistics = log.id
+    LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_SUBCATS pri
+          ON cust.custentityam_primary_sport = pri.id
+    LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_SUBCATS sec
+          ON cust.custentityam_secondary_sport = sec.id
+    LEFT JOIN netsuite.CUSTOMLISTB2B_MATRIX_SUBCATS ter
+          ON cust.custentityam_tertiary_sport = ter.id
 WHERE cust._FIVETRAN_DELETED = FALSE
   AND (parent._FIVETRAN_DELETED = FALSE OR parent._FIVETRAN_DELETED IS NULL)
