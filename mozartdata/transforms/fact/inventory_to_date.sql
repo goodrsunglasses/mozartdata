c967aa1873f26cf706a07497a59cf7c4bc0cc0cb
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-02 13:17:24 -0700

    Basic structure of the logic

---
+++ mozartdata/transforms/fact/inventory_to_date.sql
@@ -0,0 +1,16 @@
+WITH running_totals AS (SELECT location_name,
+							   transaction_created_date_pst,
+							   sku,
+							   plain_name,
+							   SUM(quantity) OVER (
+								   PARTITION BY
+									   sku,
+									   location_name,
+									   plain_name
+								   ORDER BY
+									   transaction_created_date_pst ROWS BETWEEN UNBOUNDED PRECEDING
+									   AND CURRENT ROW
+								   ) AS cumulative_quantity
+						FROM fact.inventory_item_detail)
+SELECT *
+FROM running_totals
