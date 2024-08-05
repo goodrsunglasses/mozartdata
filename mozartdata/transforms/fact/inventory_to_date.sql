92943b770c47acbb9ea9dd1d33fe18847b5a2fbb
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-05 13:02:40 -0700

    Added most recent boolean

--- mozartdata/transforms/fact/inventory_to_date.sql
+++ mozartdata/transforms/fact/inventory_to_date.sql
@@ -10,7 +10,17 @@ WITH running_totals AS (SELECT location_name,
 								   ORDER BY
 									   transaction_created_date_pst ROWS BETWEEN UNBOUNDED PRECEDING
 									   AND CURRENT ROW
-								   ) AS cumulative_quantity
+								   )                                                                           AS cumulative_quantity,
+							   ROW_NUMBER() OVER (PARTITION BY sku ORDER BY transaction_created_date_pst DESC) AS row_num
 						FROM fact.inventory_item_detail)
-SELECT *
-FROM running_totals order by location_name,transaction_created_date_pst
+SELECT location_name,
+	   transaction_created_date_pst,
+	   sku,
+	   plain_name,
+	   cumulative_quantity,
+	   CASE
+		   WHEN row_num = 1 THEN TRUE
+		   ELSE FALSE
+		   END AS is_most_recent
+FROM running_totals
+ORDER BY location_name, transaction_created_date_pst
