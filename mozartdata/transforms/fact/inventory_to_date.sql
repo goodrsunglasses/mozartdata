824969dc63b38260b3e67e2b2144db949316c26c
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-09 11:14:08 -0700

    Revamped it because just summing it all by day then adding it made more sense, the last logic was much more convoluted

--- mozartdata/transforms/fact/inventory_to_date.sql
+++ mozartdata/transforms/fact/inventory_to_date.sql
@@ -1,28 +1,34 @@
-WITH running_totals
-		 AS (SELECT --the idea here is that we wanted a table that would concat the netsuite inventory "balance sheet" into a view by sku by location UP TO a given day since it came up alot
-					location_name,
-					transaction_created_date_pst,
-					sku,
-					plain_name,
-					SUM(quantity) OVER (
-						PARTITION BY
-							sku,
-							location_name,
-							plain_name
-						ORDER BY
-							transaction_created_date_pst ROWS BETWEEN UNBOUNDED PRECEDING
-							AND CURRENT ROW
-						)                                                                           AS cumulative_quantity,
-					ROW_NUMBER() OVER (PARTITION BY sku ORDER BY transaction_created_date_pst DESC) AS row_num
-			 FROM fact.inventory_item_detail)
+WITH grouped_data AS (SELECT location_name,
+							 transaction_created_date_pst,
+							 sku,
+							 plain_name,
+							 SUM(quantity) AS daily_quantity
+					  FROM fact.inventory_item_detail detail
+					  GROUP BY location_name,
+							   transaction_created_date_pst,
+							   sku,
+							   plain_name)
 SELECT location_name,
 	   transaction_created_date_pst,
 	   sku,
 	   plain_name,
-	   cumulative_quantity,
+	   SUM(daily_quantity) OVER (
+		   PARTITION BY
+			   sku,
+			   location_name
+		   ORDER BY
+			   transaction_created_date_pst ROWS BETWEEN UNBOUNDED PRECEDING
+			   AND CURRENT ROW
+		   )   AS cumulative_quantity,
 	   CASE
-		   WHEN row_num = 1 THEN TRUE
+		   WHEN ROW_NUMBER() OVER (
+			   PARTITION BY
+				   sku,
+				   location_name
+			   ORDER BY
+				   transaction_created_date_pst DESC
+			   ) = 1 THEN TRUE
 		   ELSE FALSE
 		   END AS is_most_recent
-FROM running_totals
-ORDER BY location_name, transaction_created_date_pst
+FROM grouped_data
+ORDER BY transaction_created_date_pst DESC
