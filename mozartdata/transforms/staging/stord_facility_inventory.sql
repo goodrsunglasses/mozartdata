e4793a048bb21d5bd09bfd1bfb41a8441fb1d94f
Josha Russell <russell.josha@gmail.com>
2024-08-09 17:06:34 -0700

    added stord_facility_inventory

---
+++ mozartdata/transforms/staging/stord_facility_inventory.sql
@@ -0,0 +1,24 @@
+/*
+The point of this transform is to create an incremental build (aka snapshot) of stord_facility_inventory
+This transform is setup as an incremental build
+*/
+SELECT
+  sfi.sku
+, sfi.name                               AS display_name
+, sfi.facility_id                        AS location_id_stord
+, sfi.facility_alias                     AS location_name_stord
+, sfi.allocated
+, sfi.available
+, sfi.damaged
+, sfi.incoming
+, sfi.locked
+, sfi.base_unit
+, sfi.inventory_alerts
+, sfi.inventory_alerts:"OUT_OF_STOCK"    AS out_of_stock
+, sfi.inventory_alerts:"REORDER_WARNING" AS reorder_warning
+
+
+, sfi._portable_extracted                AS snapshot_timestamp
+, DATE(sfi._portable_extracted)          AS snapshot_date
+FROM
+  stord.STORD_FACILITY_INVENTORY_8589936822 sfi
--- mozartdata/transforms/staging/stord_inventory.sql
+++ mozartdata/transforms/staging/stord_inventory.sql
@@ -1,22 +1,29 @@
 /*
-The point of this transform is to create an incremental build (aka snapshot) of stord_facility_inventory
+The point of this transform is to create an incremental build (aka snapshot) of stord_network_inventory
 This transform is setup as an incremental build
 */
 SELECT
-  sfi.sku
-, sfi.name                               AS display_name
-, sfi.facility_id                        AS location_id_stord
-, sfi.facility_alias                     AS location_name_stord
-, sfi.allocated
-, sfi.available
-, sfi.damaged
-, sfi.incoming
-, sfi.base_unit
-, sfi.inventory_alerts
-, sfi.inventory_alerts:"OUT_OF_STOCK"    AS out_of_stock
-, sfi.inventory_alerts:"REORDER_WARNING" AS reorder_warning
-, sfi.locked
-, sfi._portable_extracted                AS snapshot_timestamp
-, DATE(sfi._portable_extracted)          AS snapshot_date
+  sni.sku
+, sni.name                               AS display_name
+, sni.unit
+, sni.network_balances
+, cast(sni.network_balances:"ACCEPTED" as integer) AS accepted
+, cast(sni.network_balances:"ALLOCATED" as integer) AS allocated
+, cast(sni.network_balances:"AVAILABLE" as integer) AS available
+, cast(sni.network_balances:"BACKORDERED" as integer) AS backordered
+, cast(sni.network_balances:"BACKORDERED_AVAILABLE" as integer) AS backordered_available
+, cast(sni.network_balances:"COMMITTED" as integer) AS committed
+, cast(sni.network_balances:"DAMAGED" as integer) AS damaged
+, cast(sni.network_balances:"INCOMING" as integer) AS incoming
+, cast(sni.network_balances:"LOCKED" as integer) AS locked
+, cast(sni.network_balances:"ON_HOLD" as integer) AS on_hold
+, cast(sni.network_balances:"OTHER" as integer) AS other
+, cast(sni.network_balances:"QUARANTINED" as integer) AS quarantined
+, cast(sni.network_balances:"RECEIVING" as integer) AS receiving
+--, cast(sni.network_balances:"RESERVATIONS" as integer) AS reservations
+, cast(sni.network_balances:"TOTAL_ON_HAND" as integer) AS total_on_hand
+, cast(sni.network_balances:"UNLOCKED" as integer) AS unlocked
+, sni._portable_extracted                AS snapshot_timestamp
+, DATE(sni._portable_extracted)          AS snapshot_date
 FROM
-  stord.stord_facility_inventory_8589936822 sfi
+  stord.stord_network_inventory_8589936822 sni
