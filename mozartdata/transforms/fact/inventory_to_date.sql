ad7139669929b371c9e3a5bebc1ca64a3eebe689
goodrbiflock <126719614+goodrbiflock@users.noreply.github.com>
2024-08-12 10:43:02 -0700

    Inventory to date (#58)
    * Basic structure of the logic
    
    * Basic structure of the logic
    
    * Added most recent boolean
    
    * Added comments
    
    * Revamped it because just summing it all by day then adding it made more sense, the last logic was much more convoluted
    
    ---------
    
    Co-authored-by: Kaden-SL <ksedmakl@ucsc.edu>

---
+++ mozartdata/transforms/fact/inventory_to_date.sql
@@ -0,0 +1,34 @@
+WITH grouped_data AS (SELECT location_name,
+							 transaction_created_date_pst,
+							 sku,
+							 plain_name,
+							 SUM(quantity) AS daily_quantity
+					  FROM fact.inventory_item_detail detail
+					  GROUP BY location_name,
+							   transaction_created_date_pst,
+							   sku,
+							   plain_name)
+SELECT location_name,
+	   transaction_created_date_pst,
+	   sku,
+	   plain_name,
+	   SUM(daily_quantity) OVER (
+		   PARTITION BY
+			   sku,
+			   location_name
+		   ORDER BY
+			   transaction_created_date_pst ROWS BETWEEN UNBOUNDED PRECEDING
+			   AND CURRENT ROW
+		   )   AS cumulative_quantity,
+	   CASE
+		   WHEN ROW_NUMBER() OVER (
+			   PARTITION BY
+				   sku,
+				   location_name
+			   ORDER BY
+				   transaction_created_date_pst DESC
+			   ) = 1 THEN TRUE
+		   ELSE FALSE
+		   END AS is_most_recent
+FROM grouped_data
+ORDER BY transaction_created_date_pst DESC
