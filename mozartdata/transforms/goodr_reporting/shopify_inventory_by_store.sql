26a6ee75f38a81f258f79e84e8a7f7ecccda5851
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-14 13:03:52 -0700

    Draft pivot

--- mozartdata/transforms/goodr_reporting/shopify_inventory_by_store.sql
+++ mozartdata/transforms/goodr_reporting/shopify_inventory_by_store.sql
@@ -1,1 +1,8 @@
-select * from staging.shopify_inventory_incremental
+select *
+from (select distinct --select the distinct skus and days that they have snapshot data
+                      sku,
+                      fivetran_snapshot_date_pst,
+                      store
+      from fact.shopify_inventory)
+    PIVOT
+( MAX(QUANTITY) FOR STORE IN ('Specialty' AS Specialty, 'HQ DC' AS HQ_DC, 'Warehouse' AS Warehouse))
