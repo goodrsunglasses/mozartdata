3c491ab866c3cbee6aed06f2268506bb4ef63031
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-14 16:37:54 -0700

    Fixed the bs names

--- mozartdata/transforms/fact/shopify_inventory.sql
+++ mozartdata/transforms/fact/shopify_inventory.sql
@@ -1,5 +1,5 @@
--- CREATE OR REPLACE TABLE fact.shopify_inventory
---            COPY GRANTS  as
+CREATE OR REPLACE TABLE fact.shopify_inventory
+           COPY GRANTS  as
 SELECT --Idea here is to select from the shopify snapshot tables native to mozart then join to some relevant fact and dim tables, and also renaming to make the fields clearer
 	   snapshot.store,
 	   snapshot.category,
@@ -7,11 +7,10 @@ SELECT --Idea here is to select from the shopify snapshot tables native to mozar
 	   snapshot.sku,
 	   snapshot.quantity,
 	   --From now on, as per convo converting to PST to keep things consistent across inventory system snapshots
-	   CONVERT_TIMEZONE('America/Los_Angeles', snapshot.FIVETRAN_SYNC_TIME_UTC)       AS FIVETRAN_SYNC_TIME_PST,
-	   DATE(CONVERT_TIMEZONE('America/Los_Angeles', snapshot.FIVETRAN_SYNC_TIME_UTC)) AS fivetran_snapshot_date_pst,
+	   snapshot.snapshot_timestamp       AS snapshot_ts,
+	   date(snapshot.snapshot_timestamp) as snapshot_date,
 	   CONVERT_TIMEZONE('America/Los_Angeles', snapshot.UPDATED_AT_UTC)               AS UPDATED_AT_SHOPIFY_PST,
 	   DATE(CONVERT_TIMEZONE('America/Los_Angeles', snapshot.UPDATED_AT_UTC))         AS UPDATED_AT_SHOPIFY_DATE_PST,
-	   CONVERT_TIMEZONE('America/Los_Angeles', snapshot.snapshot_timestamp)                  AS snapshot_ts_mozart_pst,
 	   snapshot.tracked,
 	   --everything past here is included for posterity, as often we seem to need the most nonsensical fields for one specific request
 	   snapshot.cost,
@@ -20,8 +19,10 @@ SELECT --Idea here is to select from the shopify snapshot tables native to mozar
 	   prod.family,
 	   prod.stage,
 	   prod.MERCHANDISE_CLASS,
-	   prod.DESIGN_TIER
+	   prod.DESIGN_TIER,
+	   CONVERT_TIMEZONE('America/Los_Angeles', snapshot.FIVETRAN_SYNC_TIME_UTC)       AS FIVETRAN_SYNC_TIME_PST,
+	   DATE(CONVERT_TIMEZONE('America/Los_Angeles', snapshot.FIVETRAN_SYNC_TIME_UTC)) AS fivetran_sync_date_pst
 FROM STAGING.SHOPIFY_INVENTORY_INCREMENTAL snapshot
 		 LEFT OUTER JOIN dim.product prod ON prod.sku = snapshot.sku
 WHERE tracked = TRUE --This one from what I can tell just filters out a secondary line of False, untracked item inventory?
-ORDER BY store,fivetran_snapshot_date_pst ASC
+ORDER BY store ASC
