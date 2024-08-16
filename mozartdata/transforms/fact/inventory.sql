baa3d989506a0f089a532688edfbcd508a1bdb1b
Kaden-SL <ksedmakl@ucsc.edu>
2024-08-16 14:22:26 -0700

    Made my own version of stord inventory location

--- mozartdata/transforms/fact/inventory.sql
+++ mozartdata/transforms/fact/inventory.sql
@@ -1,8 +1,14 @@
 SELECT sku,
 	   display_name,
+	   LOCATION_NAME,
 	   FIVETRAN_SNAPSHOT_DATE_PST,
-	   total_quantity_on_hand --using this one because its the highest level one available for unioning purposes
+	   total_quantity_on_hand, --using this one because its the highest level one available for unioning purposes
+'Netsuite' as source
 FROM fact.NETSUITE_INVENTORY_LOCATION
 UNION ALL
-SELECT *
-FROM FROM fact.SHOPIFY_INVENTORY
+SELECT sku,
+       display_name,
+       store,
+       snapshot_date,
+       'Shopify' as source
+FROM  fact.SHOPIFY_INVENTORY
