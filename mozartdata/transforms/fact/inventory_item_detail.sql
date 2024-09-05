fdccab0fc69cb3ed0be3859906e939caada5400a
goodrbiflock <126719614+goodrbiflock@users.noreply.github.com>
2024-09-05 12:08:01 -0700

    Inventory item detail (#67)
    * Solid start while I wait for my DB to clone
    
    * In progress
    
    * Added culmative logic, and trandate to ns inventory balance tables
    
    * Update mozartdata/transforms/staging/inventory_item_detail.sql
    
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>
    
    * Update mozartdata/transforms/fact/inventory_item_detail.sql
    
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>
    
    * Update mozartdata/transforms/fact/netsuite_inventory_item_detail.sql
    
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>
    
    * Update mozartdata/transforms/fact/inventory_item_detail.sql
    
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>
    
    * Update mozartdata/transforms/fact/inventory_item_detail.sql
    
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kaden-SL <ksedmakl@ucsc.edu>
    Co-authored-by: Josha Russell <Yoshu22@users.noreply.github.com>

---
+++ mozartdata/transforms/fact/inventory_item_detail.sql
@@ -0,0 +1,53 @@
+--CREATE OR REPLACE TABLE fact.inventory_item_detail
+--COPY GRANTS  as
+WITH netsuite_culmative
+		 AS (SELECT --the idea with this CTE is to create the culmative quantity after, and before a given inventory transaction, seperated for ease of comprehension
+					transaction_id_ns,
+					transaction_created_timestamp_pst,
+					transaction_date,
+					location_name,
+					transaction_number_ns,
+					sku,
+					quantity,
+					SUM(quantity) OVER (
+						PARTITION BY
+							sku,
+							location_name
+						ORDER BY
+							transaction_date,transaction_created_timestamp_pst ROWS BETWEEN UNBOUNDED PRECEDING --This is a bit weird, we're ordering by transaction_date first because it can be backdated-
+						-- and is technically more correct, but its not hour/minute specific which can lead to disordering so we use the timestamp also
+							AND CURRENT ROW
+						)                AS culmative,
+					culmative - quantity AS before
+			 FROM fact.NETSUITE_INVENTORY_ITEM_DETAIL)
+SELECT detail.transaction_id_ns,
+	   detail.ORDER_ID_EDW,
+	   detail.RECORD_TYPE,
+	   detail.channel,
+	   NULL      AS reason,
+	   detail.sku,
+	   detail.plain_name,
+	   detail.quantity,
+	   before    AS quantity_before,
+	   culmative AS quantity_after,
+	   detail.location_name,
+	   detail.TRANSACTION_CREATED_TIMESTAMP_PST,
+	   detail.transaction_date
+FROM fact.NETSUITE_INVENTORY_ITEM_DETAIL detail
+		 LEFT OUTER JOIN netsuite_culmative culm ON (culm.TRANSACTION_ID_NS = detail.transaction_id_ns AND culm.sku =
+																										   detail.sku) --Double join because 1 transaction can and will impact inventory for multiple skus
+UNION ALL
+SELECT detail.ADJUSTMENT_SEQUENCE,
+	   detail.order_id_ns,
+	   detail.reason_type,
+	   NULL AS channel,
+	   detail.reason,
+	   detail.sku,
+	   detail.DISPLAY_NAME,
+	   detail.adjustment_quantity,
+	   detail.PREVIOUS_QUANTITY,
+	   detail.UPDATED_QUANTITY,
+	   detail.LOCATION_NAME_STORD,
+	   ADJUSTED_TIMESTAMP,
+	   adjusted_date
+FROM fact.STORD_INVENTORY_ITEM_DETAIL detail
--- mozartdata/transforms/fact/netsuite_inventory_item_detail.sql
+++ mozartdata/transforms/fact/netsuite_inventory_item_detail.sql
@@ -1,17 +1,18 @@
---CREATE OR REPLACE TABLE fact.inventory_item_detail
-            --COPY GRANTS  as
+-- CREATE OR REPLACE TABLE fact.netsuite_inventory_item_detail
+-- 	COPY GRANTS AS
 SELECT--the idea of this table is to link the staging inventory data to other dims and facts, like parent transactions, locations, etc... and provide a solid basis to build other facts off of
 	  staging.transaction_id_ns,
-      staging.transaction_line_id_ns,
+	  staging.transaction_line_id_ns,
 	  parents.ORDER_ID_EDW,
-	    staging.RECORD_TYPE,
-	  chan.name as channel,
+	  staging.RECORD_TYPE,
+	  chan.name                          AS channel,
 	  chan.CUSTOMER_CATEGORY,
 	  chan.model,
 	  staging.location_id_ns,
-	  loc.name as location_name,
+	  loc.name                           AS location_name,
 	  staging.TRANSACTION_CREATED_TIMESTAMP_PST,
 	  staging.TRANSACTION_CREATED_DATE_PST,
+	  staging.transaction_date,
 	  staging.transaction_number_ns,
 	  staging.item_id_ns,
 	  staging.plain_name,
@@ -21,7 +22,7 @@ SELECT--the idea of this table is to link the staging inventory data to other di
 	  staging.expense_account_id_ns,
 	  staging.rate,
 	  staging.rateamount,
-	  staging.INVENTORYREPORTINGLOCATION as inventory_reporting_location,
+	  staging.INVENTORYREPORTINGLOCATION AS inventory_reporting_location,
 	  staging.customer_id_ns,
 	  staging.createdfrom
 FROM staging.INVENTORY_ITEM_DETAIL staging
