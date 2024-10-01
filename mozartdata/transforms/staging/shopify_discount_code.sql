f3c8d75808cb7b7c3f2bf81a1f137af96c318e2a
Josha Russell <russell.josha@gmail.com>
2024-10-01 10:14:28 -0700

    created staging.shopify_discount_code

---
+++ mozartdata/transforms/staging/shopify_discount_code.sql
@@ -0,0 +1,65 @@
+/*
+Create a staging table for discount_code to union all the shopify stores.
+excluding Goodrwill. That store doesn't have the other discount tables- discount allocation and discount_application
+*/
+
+SELECT
+  'Goodr.com' AS store
+, 'D2C'       AS category
+, dc.id as discount_code_id_shopify
+, dc.price_rule_id
+, dc.code as discount_code
+, dc.created_at as created_timestamp
+, date(dc.created_at) as created_date
+, dc.updated_at as updated_timestamp
+, date(dc.updated_at) as updated_date
+, dc.usage_count
+, dc._fivetran_synced
+FROM
+  shopify.discount_code dc
+UNION ALL
+SELECT
+  'Specialty' as store
+, 'B2B' as category
+, dc.id as discount_code_id_shopify
+, dc.price_rule_id
+, dc.code as discount_code
+, dc.created_at as created_timestamp
+, date(dc.created_at) as created_date
+, dc.updated_at as updated_timestamp
+, date(dc.updated_at) as updated_date
+, dc.usage_count
+, dc._fivetran_synced
+FROM
+  specialty_shopify.discount_code dc
+UNION ALL
+SELECT
+  'Goodr.ca' as store
+, 'D2C' as category
+, dc.id as discount_code_id_shopify
+, dc.price_rule_id
+, dc.code as discount_code
+, dc.created_at as created_timestamp
+, date(dc.created_at) as created_date
+, dc.updated_at as updated_timestamp
+, date(dc.updated_at) as updated_date
+, dc.usage_count
+, dc._fivetran_synced
+FROM
+  goodr_canada_shopify.discount_code dc
+UNION ALL
+SELECT
+  'Specialty CAN' as store
+, 'B2B' as category
+, dc.id as discount_code_id_shopify
+, dc.price_rule_id
+, dc.code as discount_code
+, dc.created_at as created_timestamp
+, date(dc.created_at) as created_date
+, dc.updated_at as updated_timestamp
+, date(dc.updated_at) as updated_date
+, dc.usage_count
+, dc._fivetran_synced
+FROM
+  sellgoodr_canada_shopify.discount_code dc
+
