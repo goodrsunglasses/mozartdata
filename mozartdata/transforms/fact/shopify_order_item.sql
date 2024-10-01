e3c24ad717962bc7ebc19cb0e08c96ee7e2442de
Josha Russell <russell.josha@gmail.com>
2024-10-01 11:57:59 -0700

    added amount_discount to shopify_order_item

--- mozartdata/transforms/fact/shopify_order_item.sql
+++ mozartdata/transforms/fact/shopify_order_item.sql
@@ -12,11 +12,13 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  da.amount as amount_discount,
   line.fulfillment_status
 FROM
   shopify."ORDER" d2c_shop
   LEFT OUTER JOIN shopify.order_line line ON line.order_id = d2c_shop.id
   LEFT OUTER JOIN dim.product product ON product.product_id_d2c_shopify = line.product_id
+  LEFT OUTER JOIN staging.shopify_discount_allocation da ON da.order_line_id = line.id and da.store = 'Goodr.com'
 UNION ALL
 SELECT
   b2b_shop.name order_id_edw,
@@ -32,12 +34,14 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  da.amount as amount_discount,
   line.fulfillment_status
 FROM
   specialty_shopify."ORDER" b2b_shop
   LEFT OUTER JOIN specialty_shopify.order_line line ON line.order_id = b2b_shop.id
   LEFT OUTER JOIN dim.product product ON product.product_id_b2b_shopify = line.product_id
-  UNION ALL
+  LEFT OUTER JOIN staging.shopify_discount_allocation da ON da.order_line_id = line.id and da.store = 'Specialty'
+UNION ALL
 SELECT
   b2b_shop.name order_id_edw,
   b2b_shop.id order_id_shopify,
@@ -52,6 +56,7 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  0 as amount_discount,
   line.fulfillment_status
 FROM
   goodrwill_shopify."ORDER" b2b_shop
@@ -72,11 +77,13 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  da.amount as amount_discount,
   line.fulfillment_status
 FROM
   sellgoodr_canada_shopify."ORDER" b2b_shop
   LEFT OUTER JOIN sellgoodr_canada_shopify.order_line line ON line.order_id = b2b_shop.id
   LEFT OUTER JOIN dim.product product ON product.sku = line.sku
+  LEFT OUTER JOIN staging.shopify_discount_allocation da ON da.order_line_id = line.id and da.store = 'Specialty CAN'
   UNION ALL
 SELECT
   b2b_shop.name order_id_edw,
@@ -92,11 +99,13 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  da.amount as amount_discount,
   line.fulfillment_status
 FROM
   goodr_canada_shopify."ORDER" b2b_shop
   LEFT OUTER JOIN goodr_canada_shopify.order_line line ON line.order_id = b2b_shop.id
   LEFT OUTER JOIN dim.product product ON product.sku = line.sku
+  LEFT OUTER JOIN staging.shopify_discount_allocation da ON da.order_line_id = line.id and da.store = 'Goodr.ca'
   UNION ALL
 SELECT
   b2b_shop.name order_id_edw,
@@ -112,8 +121,10 @@ SELECT
   line.fulfillable_quantity as quantity_unfulfilled,
   line.price * line.quantity as amount_booked,
   line.price * (line.quantity - line.fulfillable_quantity) as amount_sold,
+  da.amount as amount_discount,
   line.fulfillment_status
 FROM
   cabana."ORDER" b2b_shop
   LEFT OUTER JOIN cabana.order_line line ON line.order_id = b2b_shop.id
   LEFT OUTER JOIN dim.product product ON product.sku = line.sku
+  LEFT OUTER JOIN staging.shopify_discount_allocation da ON da.order_line_id = line.id and da.store = 'Cabana'
