7a51a7c12ee78e18c2e200a9ceaa6a8d8e0dc129
Kaden-SL <ksedmakl@ucsc.edu>
2024-10-21 12:25:18 -0700

    Pretty much finalized the NS portion of this

--- mozartdata/transforms/staging/netsuite_refund_item_detail.sql
+++ mozartdata/transforms/staging/netsuite_refund_item_detail.sql
@@ -1,12 +1,12 @@
-CREATE OR REPLACE TABLE staging.netsuite_refund_item_detail
-          COPY GRANTS as
-SELECT
-	   transaction as transaction_id_ns,
-       tranline.id as transaction_line_id_ns,
+-- CREATE OR REPLACE TABLE staging.netsuite_refund_item_detail
+-- 	COPY GRANTS AS
+SELECT transaction                                               AS transaction_id_ns,
+	   tranline.id                                               AS transaction_line_id_ns,
 	   REPLACE(
 			   COALESCE(
 					   tran.custbody_goodr_shopify_order,
-					   tran.custbody_goodr_po_number
+					   tran.custbody_goodr_po_number,
+					   CASE WHEN tranline.CREATEDFROM IS NULL THEN tran.tranid end-- added this in for all the one off ASC refunds with no order number or parents
 			   ),
 			   ' ',
 			   ''
@@ -15,18 +15,18 @@ SELECT
 	   DATE(
 			   CONVERT_TIMEZONE('America/Los_Angeles', tran.createddate)
 	   )                                                         AS transaction_created_date_pst,
-	date(tran.trandate) as transaction_date,
+	   DATE(tran.trandate)                                       AS transaction_date,
 	   tran.recordtype                                           AS record_type,
 	   tran.memo,
-	   tranline.memo as line_memo,
-	   tran.tranid as transaction_number_ns,
-	   tranline.entity as customer_id_ns,
-	   tranline.item as item_id_ns,
+	   tranline.memo                                             AS line_memo,
+	   tran.tranid                                               AS transaction_number_ns,
+	   tranline.entity                                           AS customer_id_ns,
+	   tranline.item                                             AS item_id_ns,
 	   COALESCE(item.displayname, item.externalid)               AS plain_name,
-	   tranline.cseg7 as channel_id_ns,
+	   tranline.cseg7                                            AS channel_id_ns,
 	   tranline.quantity,
 	   tranline.itemtype,
-	   tranline.expenseaccount as expense_account_id_ns,
+	   tranline.expenseaccount                                   AS expense_account_id_ns,
 	   tranline.rate,
 	   tranline.rateamount,
 	   tranline.createdfrom
