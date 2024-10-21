24b213e749e21c3683bdf627f9ffaf17d8c5e65e
Kaden-SL <ksedmakl@ucsc.edu>
2024-10-21 12:08:48 -0700

    Changed the quantity sum to only include specific items

--- mozartdata/transforms/fact/netsuite_refund_line.sql
+++ mozartdata/transforms/fact/netsuite_refund_line.sql
@@ -1,5 +1,24 @@
 -- CREATE OR REPLACE TABLE fact.netsuite_refund_line
 -- 	COPY GRANTS AS
 SELECT transaction_id_ns,
+	   order_id_edw,
+	   channel,
+	   customer_category,
+	   transaction_created_date_pst,
+	   transaction_date,
+	   transaction_number_ns,
+	   memo,
+	   SUM(quantity)        total_quantity,
+	   SUM(rate)            total_rate,
+	   SUM(RATEAMOUNT)      total_rateamount,
+	   ARRAY_AGG(line_memo) line_memos
 
 FROM fact.netsuite_refund_item_detail
+GROUP BY transaction_id_ns,
+		 order_id_edw,
+		 channel,
+		 customer_category,
+		 transaction_created_date_pst,
+		 transaction_date,
+		 transaction_number_ns,
+		 memo
