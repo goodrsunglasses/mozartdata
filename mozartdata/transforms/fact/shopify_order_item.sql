7e0de7261f87034816d7969fc9f58ae929185975
Josha Russell <russell.josha@gmail.com>
2024-10-28 14:32:13 -0700

    aggregate shopify_order_item

--- mozartdata/transforms/fact/shopify_order_item.sql
+++ mozartdata/transforms/fact/shopify_order_item.sql
@@ -1,25 +1,27 @@
-SELECT o.order_id_edw,
-	   o.order_id_shopify,
-	   o.store,
-	   line.order_line_id_shopify,
-	   line.product_id_shopify,
-	   line.sku                                                                AS product_id_edw,
-	   line.sku,
-	   line.display_name,
-	   line.price                                                              AS rate,
-	   line.quantity                                                           AS quantity_booked,
-	   line.quantity - line.fulfillable_quantity                               AS quantity_sold,
-	   line.fulfillable_quantity                                               AS quantity_unfulfilled,
-	   line.price * line.quantity                                              AS amount_booked,
-	   line.price * (line.quantity - line.fulfillable_quantity)                AS amount_sold,
-	  da.amount_standard_discount,
-	  amount_yotpo_discount,
-	   amount_total_discount,
-	   line.fulfillment_status
-FROM staging.shopify_orders o
-		 LEFT OUTER JOIN staging.shopify_order_line line
-						 ON line.order_id_shopify = o.order_id_shopify AND line.store = o.store
-		 LEFT OUTER JOIN fact.shopify_discount_item da
-						 ON da.order_line_id_shopify = line.order_line_id_shopify AND da.store = o.store
-		 LEFT OUTER JOIN dim.product p ON p.product_id_edw = line.sku
+SELECT
+  o.order_id_edw
+, o.order_id_shopify
+, o.store
+, line.order_line_id_shopify
+, line.product_id_shopify
+, line.sku                                                 AS product_id_edw
+, line.sku
+, line.display_name
+, line.price                                               AS rate
+, line.quantity                                            AS quantity_booked
+, line.quantity - line.fulfillable_quantity                AS quantity_sold
+, line.fulfillable_quantity                                AS quantity_unfulfilled
+, line.price * line.quantity                               AS amount_booked
+, line.price * (line.quantity - line.fulfillable_quantity) AS amount_sold
+, SUM(da.amount_standard_discount)                         AS amount_standard_discount
+, SUM(da.amount_yotpo_discount)                            AS amount_yotpo_discount
+, SUM(da.amount_total_discount)                            AS amount_total_discount
+, line.fulfillment_status
+FROM
+  staging.shopify_orders o
+  LEFT OUTER JOIN staging.shopify_order_line line
+                  ON line.order_id_shopify = o.order_id_shopify AND line.store = o.store
+  LEFT OUTER JOIN fact.shopify_discount_item da
+                  ON da.order_line_id_shopify = line.order_line_id_shopify AND da.store = o.store
+  LEFT OUTER JOIN dim.product p ON p.product_id_edw = line.sku
 GROUP BY ALL
