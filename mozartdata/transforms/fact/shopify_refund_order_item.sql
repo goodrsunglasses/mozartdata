b93e32e11ada4bdec2c64683db6c346d3b01d12d
Josha Russell <russell.josha@gmail.com>
2024-11-06 20:52:17 -0800

    more documentation

--- mozartdata/transforms/fact/shopify_refund_order_item.sql
+++ mozartdata/transforms/fact/shopify_refund_order_item.sql
@@ -2,6 +2,7 @@
 This table may seem incredibly perfunctory, but we've been burned in the past by assuming "We won't need SPECIFICALLY shopify refund item level information"
 So it exists to just play it safe, and keep us Scaleagile. We are SPECIFICALLY avoiding Adjustment info here, because it does not tie to a specific item on the order
 We are having to pull display name from dim.product because in some cases a sku can have multiple display names in shopify.
+This table is 1 row per store, refund, order and sku combination. A single order can have multiple refunds.
 */
 SELECT
     concat(roid.store,'_',roid.refund_id_shopify,'_',roid.order_id_edw,'_',roid.sku) refund_order_item_id_edw, --primary_key
