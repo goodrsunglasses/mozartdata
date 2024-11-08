c0ea277ca83088dea926ec8888f7207a8fd99b13
Josha Russell <russell.josha@gmail.com>
2024-11-07 16:23:21 -0800

    adding refund info

--- mozartdata/transforms/fact/shopify_order_item.sql
+++ mozartdata/transforms/fact/shopify_order_item.sql
@@ -9,22 +9,28 @@ Most are from 2019, largely due to some oddity where there are multiple order_id
 SELECT
   o.order_id_edw
 , o.order_id_shopify
-, concat(o.store,'_',o.order_id_edw,'_',line.sku) as order_item_id_edw --primary key
+, CONCAT(o.store, '_', o.order_id_edw, '_', line.sku)                                                            AS order_item_id_edw --primary key
 , o.store
 , line.product_id_shopify
-, line.sku                                                 AS product_id_edw
+, line.sku                                                                                                       AS product_id_edw
 , line.sku
 , line.display_name
-, avg(line.price)                                               AS rate
-, sum(line.quantity)                                            AS quantity_booked
-, sum(line.quantity - line.fulfillable_quantity)                AS quantity_sold
-, sum(line.fulfillable_quantity)                                AS quantity_unfulfilled
-, sum(case when line.fulfillment_status = 'fulfilled' then line.quantity - line.fulfillable_quantity else 0 end) as quantity_fulfilled
-, sum(line.price * line.quantity)                          AS amount_booked
-, sum(line.price * (line.quantity - line.fulfillable_quantity)) AS amount_sold
-, SUM(coalesce(da.amount_standard_discount,0))                         AS amount_standard_discount
-, SUM(coalesce(da.amount_yotpo_discount,0))                            AS amount_yotpo_discount
-, SUM(coalesce(da.amount_total_discount,0))                            AS amount_total_discount
+, AVG(line.price)                                                                                                AS rate
+, SUM(line.quantity)                                                                                             AS quantity_booked
+, SUM(line.quantity - line.fulfillable_quantity)                                                                 AS quantity_sold
+, SUM(line.fulfillable_quantity)                                                                                 AS quantity_unfulfilled
+, SUM(CASE
+        WHEN line.fulfillment_status = 'fulfilled' THEN line.quantity - line.fulfillable_quantity
+        ELSE 0 END)                                                                                              AS quantity_fulfilled
+, SUM(line.price * line.quantity)                                                                                AS amount_booked
+, SUM(line.price * (line.quantity - line.fulfillable_quantity))                                                  AS amount_sold
+, SUM(COALESCE(da.amount_standard_discount, 0))                                                                  AS amount_standard_discount
+, SUM(COALESCE(da.amount_yotpo_discount, 0))                                                                     AS amount_yotpo_discount
+, SUM(COALESCE(da.amount_total_discount, 0))                                                                     AS amount_total_discount
+, SUM(COALESCE(rol.quantity_refund_line, 0))                                                                     AS quantity_refund_line
+, SUM(rol.amount_refund_subtotal)                                                                                AS amount_refund_subtotal
+, SUM(rol.amount_refund_tax)                                                                                     AS amount_refund_tax
+, SUM(rol.amount_refund_total)                                                                                   AS amount_refund_total
 FROM
   staging.shopify_orders o
   LEFT OUTER JOIN staging.shopify_order_line line
@@ -32,5 +38,7 @@ FROM
   LEFT OUTER JOIN fact.shopify_discount_item da
                   ON da.order_line_id_shopify = line.order_line_id_shopify AND da.store = o.store
   LEFT OUTER JOIN dim.product p ON p.product_id_edw = line.sku
+  LEFT OUTER JOIN fact.shopify_refund_order_line rol
+                  ON line.order_line_id_shopify = rol.order_line_id_shopify AND o.store = rol.store
 GROUP BY ALL
 
