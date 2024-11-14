25968ac6dfc17edcfaeb69f07d97316e2bbd3a9a
Josha Russell <russell.josha@gmail.com>
2024-11-13 22:54:44 -0800

    discount cleanup, adding sales

--- mozartdata/transforms/fact/shopify_orders.sql
+++ mozartdata/transforms/fact/shopify_orders.sql
@@ -25,18 +25,17 @@ SELECT DISTINCT
   o.store,
   o.email,
   o.customer_id as customer_id_shopify,
+  SUM(line.quantity_sold)  as quantity_sold,
+  SUM(line.quantity_booked) as quantity_booked,
   o.total_line_items_price as amount_booked,
   coalesce(ship.price,0) as shipping_sold,
   o.amount_tax_sold as amount_tax_sold,
   o.amount_sold as amount_sold,
   round(o.amount_sold - o.amount_tax_sold - coalesce(ship.price,0) + o.amount_discount,2) as amount_product_sold,
   round(o.amount_sold - o.amount_tax_sold - gc.amount_gift_card, 2) as amount_sales, --similar to revenue
-  o.amount_discount*-1 as amount_discount,
   sum(line.amount_yotpo_discount) as amount_yotpo_discount,
   sum(line.amount_standard_discount) as amount_standard_discount,
   sum(line.amount_total_discount) as amount_total_discount,
-  round(o.amount_sold - o.amount_tax_sold - coalesce(ship.price,0) + o.amount_discount,2) as amount_product_sold,
-  o.amount_discount*-1 as amount_discount,
   coalesce(r.amount_refunded,0) as amount_refunded,
   o.created_at as order_created_timestamp,
   DATE(o.created_at) as order_created_date,
@@ -49,12 +48,10 @@ SELECT DISTINCT
   o.cart_token,
   o.token,
   o.checkout_token,
-  o.checkout_id as checkout_id_shopify,
-  SUM(line.quantity_sold)  as quantity_sold,
-  SUM(line.quantity_booked) as quantity_booked
+  o.checkout_id as checkout_id_shopify
 FROM
   staging.shopify_orders o
-  LEFT OUTER JOIN staging.shopify_order_line line ON line.order_id_shopify = o.order_id_shopify and o.store = line.store
+  LEFT OUTER JOIN fact.shopify_order_line line ON line.order_id_shopify = o.order_id_shopify and o.store = line.store
   LEFT OUTER JOIN staging.shopify_order_shipping_line ship ON ship.order_id_shopify = o.order_id_shopify and o.store = ship.store
   LEFT OUTER JOIN gift_cards gc ON o.ORDER_ID_EDW = gc.ORDER_ID_EDW
   LEFT OUTER JOIN refunds r on o.order_id_shopify = r.order_id_shopify
