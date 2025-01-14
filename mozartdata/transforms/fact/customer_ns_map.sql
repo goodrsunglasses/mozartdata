76048e6fe61bd213e2c017819e489b8245fe1798
Josha Russell <russell.josha@gmail.com>
2025-01-13 16:41:04 -0800

    bring in 2024 tiers

--- mozartdata/transforms/fact/customer_ns_map.sql
+++ mozartdata/transforms/fact/customer_ns_map.sql
@@ -1,9 +1,13 @@
 SELECT
   cust.customer_id_edw,
-  nc.*
+  nc.*,
+  ct.tier as tier_2024
 FROM
   dim.CUSTOMER cust
 CROSS JOIN LATERAL FLATTEN(INPUT => cust.CUSTOMER_ID_NS) AS ns_ids
 LEFT OUTER JOIN
       staging.netsuite_customers nc
       ON nc.customer_id_ns = ns_ids.value
+LEFT OUTER JOIN
+    staging.customer_tier_snapshot_2024 ct
+    on cust.customer_id_edw = ct.customer_id_edw
