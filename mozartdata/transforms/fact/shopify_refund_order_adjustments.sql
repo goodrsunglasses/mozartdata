45a30f2421b9afcf18cee4dab5e1cf551dfca96f
goodrsamwise <samnickolls@goodr.com>
2025-01-16 12:07:56 -0800

    Updated fact.shopify_refund_order_adjustments to match the change in the other shopify refund table.

--- mozartdata/transforms/fact/shopify_refund_order_adjustments.sql
+++ mozartdata/transforms/fact/shopify_refund_order_adjustments.sql
@@ -1,14 +1,17 @@
-SELECT
-       ref.refund_id as refund_id_shopify,
-       ref.order_id as order_id_shopify,
-       o.order_id_edw,
-       ref.created_at,
-       ref.store,
-       ref.amount*-1 as amount_adjustment,
-       ref.tax_amount*-1 as amount_adjustment_tax,
-       ref.total_adj_amnt*-1    as amount_adjustment_total,
-       ref.note,
-       ref.reason
+select
+    ref.refund_id           as refund_id_shopify
+    , ref.order_id            as order_id_shopify
+    , o.order_id_edw
+    , ref.created_at
+    , ref.store
+    , ref.amount * -1         as amount_adjustment
+    , ref.tax_amount * -1     as amount_adjustment_tax
+    , ref.total_adj_amnt * -1 as amount_adjustment_total
+    , ref.note
+    , ref.reason
 from
-  staging.shopify_refund_order_adjustments ref
-  left join dim.orders o on ref.order_id = o.order_id_shopify
+    staging.shopify_refund_order_adjustments as ref
+left join
+    staging.shopify_orders                   as o
+    on
+    o.order_id_shopify = ref.order_id
