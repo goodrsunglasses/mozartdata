98f68aabe355f208a4876a037714519cbdf7170c
goodrsamwise <samnickolls@goodr.com>
2025-02-07 19:29:20 -0800

    Updated to use the Shopify export from 2-6-2025.
    Added minor documentation.
    
    Much more to come over the weekend.

--- mozartdata/transforms/goodr_reporting/goodr_com_usage_kpis.sql
+++ mozartdata/transforms/goodr_reporting/goodr_com_usage_kpis.sql
@@ -1,40 +1,35 @@
-with sessions_cte as (
-     select
-         ses.date
-       , ses.session_default_channel_grouping as channel_group
-       , sum(zeroifnull(ses.sessions))        as total_sessions
-     from
-         google_analytics_4.traffic_acquisition_session_default_channel_grouping as ses
-     where
-         ses.property = 'properties/353637527'
-     group by
-         ses.date
-       , channel_group
-     order by
-         ses.date desc
-)
+/*
+    This table shows the daily KPIs for goodr.com usage.
+    Columns:
+        date:
+            Date reported on. This comes from dim.date to prevent issues
+            with a date not existing in Shopify.
+        sessions:
+            Total sessions as defined by Shopify for each day.
+        users:
+            Labeled "Online store visitors" in Shopify, this is the number of
+            unique users that visited the site wach day regardless of their
+            session count.
+        total_customers:
+            This is the total number of users that bought a product in a day.
 
-, conversions_cte as (
-    select
-        e.date
-        , sum(e.total_users) as total_conversions
-    from
-        google_analytics_4.events as e
-    where
-        property = 'properties/353637527'
-        and lower(event_name) = 'purchase'
-    group by
-        e.date
-    order by
-        date desc
-)
+    More to come
+
+ */
 
 select
-    s.date
-    , s.total_sessions
-    , c.total_conversions
-    , round((c.total_conversions / s.total_sessions), 2) as conv_rate
+    d.date::date                                    as date
+    , shopify.sessions
+    , shopify."ONLINE STORE VISITORS"               as users
+    , shopify."NEW CUSTOMERS"                       as new_customers
+    , shopify.customers                             as total_customers
+    , shopify."SESSIONS THAT COMPLETED CHECKOUT"    as sess_completed_checkout
+    , shopify."CONVERSION RATE"                     as daily_conversion_rate
 from
-    sessions_cte as s
-left join
-    conversions_cte as c
+    dim.date                                        as d
+inner join
+    shopify_exports.kpi_data_20240101_20250206      as shopify
+    on
+        d.date = shopify.day::date
+where
+    d.date >= '2024-01-01'
