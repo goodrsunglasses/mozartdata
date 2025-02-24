1ca95ab877581c557c31d10a0eaec56a6aa83815
goodrsamwise <samnickolls@goodr.com>
2025-02-24 11:15:52 -0800

    Updated comments. Removed code used for getting product sales until KRH decide what to use. QC will be added in github directly as it has to be done with the front end systems.
    To document this somewhere, I think using Shopify for product_sales will result in having a number that doesn't correlate with our finance reporting, which will eventually cause confusion and lead to this eventually being changed to use our finance tables (like what I've done), but being done by someone else (meaning I don't get the experience). I could be wrong, but if not I would like to note it here that I have mentioned it.

--- mozartdata/transforms/dev_reporting/goodr_com_usage_kpis.sql
+++ mozartdata/transforms/dev_reporting/goodr_com_usage_kpis.sql
@@ -1,27 +1,32 @@
 /*
     This table shows the daily KPIs for goodr.com usage.
     Columns:
-        date:
+        event_date:
             Date reported on. This comes from dim.date to prevent issues
             with a date not existing in Shopify.
-        sessions:
+        shopify_sessions:
             Total sessions as defined by Shopify for each day.
-        users:
+        shopify_users:
             Labeled "Online store visitors" in Shopify, this is the number of
             unique users that visited the site wach day regardless of their
             session count.
-        total_customers:
+        shopify_total_customers:
             This is the total number of users that bought a product in a day.
-        new_customers:
+        shopify_new_customers:
             This is the number of new users that bought a product in a day.
-        existing_customers:
-
-  , shopify.sessions_completed_checkout                    as shopify_sessions_completed_checkout
-  , marketing.total_spend                                  as marketing_spend
-  , marketing.total_impressions                            as marketing_impressions
-  , gl_tran.total_product_sales                                                  as existing_customer_product_sales
-  , ifnull(yotpo_redemptions.redeeming_customers, 0)       as yotpo_redeeming_customers
-  , ifnull(yotpo_accounts.daily_total_created_accounts, 0) as yotpo_accounts_created
+        shopify_existing_customers:
+            This is the number of users that aren't new that bought a product in a day.
+        shopify_sessions_completed_checkout:
+            This is the number of sessions that ended up buying a product in a day.
+        marketing_spend:
+            This is the total spend on marketing across the platforms tracked in goodr_reporting.performance_media.
+        marketing_impressions:
+            This is the total amount of impressions (basically people tha saw an ad) across marketing platforms.
+        yotpo_redeeming_customers:
+            This is the total number of customers that redeemed points in a day.
+                - This may include Canada, I will find out from Jared soon
+        yotpo_accounts_created:
+            This is the total number of accounts created in a day.
     Todos:
         - handle leap years
  */
@@ -41,23 +46,24 @@ with
                             group by
                                 date
                         )
-  , product_sales_totals as (
-                            select
-                                gl_tran.transaction_date          as date
-                              , round(sum(gl_tran.net_amount), 2) as total_product_sales
-                            from
-                                dev_reporting.gl_transaction as gl_tran
-                            where
-                                  gl_tran.posting_flag = true
-                              and gl_tran.channel = 'Goodr.com'
-                              and gl_tran.account_number in (
-                                                             4000, 4110, 4210
-                                )
-                              and gl_tran.transaction_date >= '2024-01-01'
-                              and gl_tran.transaction_date <= current_date
-                            group by
-                                gl_tran.transaction_date
-                        )
+  -- removed until we know what we want to use for product_sales totals as a source of truth
+  -- , product_sales_totals as (
+  --                           select
+  --                               gl_tran.transaction_date          as date
+  --                             , round(sum(gl_tran.net_amount), 2) as total_product_sales
+  --                           from
+  --                               dev_reporting.gl_transaction as gl_tran
+  --                           where
+  --                                 gl_tran.posting_flag = true
+  --                             and gl_tran.channel = 'Goodr.com'
+  --                             and gl_tran.account_number in (
+  --                                                            4000, 4110, 4210
+  --                               )
+  --                             and gl_tran.transaction_date >= '2024-01-01'
+  --                             and gl_tran.transaction_date <= current_date
+  --                           group by
+  --                               gl_tran.transaction_date
+  --                       )
     -- removed as it is not used in any KPIs at the moment, but may be in the future
     -- , new_customer_revenue_totals as (
     --                           select
@@ -95,17 +101,17 @@ with
     --                       )
   , yotpo_loyalty_account_totals as (
                             select
-                                platform_account_created_at_date as date
+                                created_at_date as date
                               , count(*)                         as daily_total_created_accounts
                             from
                                 staging.yotpo_accounts_kpi_aggregation
                             where
-                                platform_account_created_at_date is not null
+                                created_at_date is not null
                             group by
-                                platform_account_created_at_date
+                                created_at_date
                         )
 select
-    d.date                                                 as date
+    d.date                                                 as event_date
   , shopify.sessions                                       as shopify_sessions
   , shopify.users                                          as shopify_users
   , shopify.total_customers                                as shopify_total_customers
@@ -114,13 +120,13 @@ select
   , shopify.sessions_completed_checkout                    as shopify_sessions_completed_checkout
   , marketing.total_spend                                  as marketing_spend
   , marketing.total_impressions                            as marketing_impressions
-  , gl_tran.total_product_sales
--- left in for potential future use for new vs existing customer product_sales
--- , new_customer_sales.new_customer_product_sales
--- , round(
---       (gl_tran.total_revenue - new_customer_sales.new_customer_product_sales)
---       , 2
---   )                                                      as existing_customer_product_sales
+  -- , gl_tran.total_product_sales
+  -- left in for potential future use for new vs existing customer product_sales
+  -- , new_customer_sales.new_customer_product_sales
+  -- , round(
+  --       (gl_tran.total_revenue - new_customer_sales.new_customer_product_sales)
+  --       , 2
+  --   )                                                      as existing_customer_product_sales
   , ifnull(yotpo_redemptions.redeeming_customers, 0)       as yotpo_redeeming_customers
   , ifnull(yotpo_accounts.daily_total_created_accounts, 0) as yotpo_accounts_created
 from
@@ -133,15 +139,15 @@ from
         marketing_totals                          as marketing
             on
             d.date = marketing.date
-    left join
-        product_sales_totals                            as gl_tran
-            on
-            d.date = gl_tran.date
--- removed as it is not used in KPIs at the moment, but may be used in the future for product_sales
--- left join
---     new_customer_revenue_totals               as new_customer_sales
---         on
---         d.date = new_customer_sales.date
+    -- left join
+    --     product_sales_totals                            as gl_tran
+    --         on
+    --         d.date = gl_tran.date
+    -- removed as it is not used in KPIs at the moment, but may be used in the future for product_sales
+    -- left join
+    --     new_customer_revenue_totals               as new_customer_sales
+    --         on
+    --         d.date = new_customer_sales.date
     left join
         staging.yotpo_redemptions_kpi_aggregation as yotpo_redemptions
             on
@@ -153,5 +159,3 @@ from
 where
       d.date >= '2024-01-01'
   and d.date <= current_date
-order by
-    d.date asc
