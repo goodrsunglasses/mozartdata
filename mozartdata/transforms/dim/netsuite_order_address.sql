13faa633262984f583918d2a977fc793a79104f0
Josha Russell <russell.josha@gmail.com>
2025-03-04 07:10:08 -0800

    move renaming to final query

--- mozartdata/transforms/dim/netsuite_order_address.sql
+++ mozartdata/transforms/dim/netsuite_order_address.sql
@@ -1,120 +1,119 @@
 with base as (
   select
-    a.nkey as order_address_id_ns
+    a.nkey
   , 'invoice' as record_type
-  , a.addr1 as address_1
-  , a.addr2 as address_2
-  , a.addr3 as address_3
-  , a.addressee as customer_name
-  , a.addrphone as phone_number
-  , a.addrtext as address_text
-  , a.attention as attention
+  , a.addr1
+  , a.addr2
+  , a.addr3
+  , a.addressee
+  , a.addrphone
+  , a.addrtext
+  , a.attention
   , a.city
   , a.state
   , a.country
   , a.zip
   , a.override
-  , a.dropdownstate as state_drop_down
+  , a.dropdownstate
   from
     netsuite.invoiceshippingaddress a
   where
     _fivetran_deleted = false
   union all
   select
-    a.nkey as order_address_id_ns
+    a.nkey
   , 'cashsale' as record_type
-  , a.addr1 as address_1
-  , a.addr2 as address_2
-  , a.addr3 as address_3
-  , a.addressee as customer_name
-  , a.addrphone as phone_number
-  , a.addrtext as address_text
-  , a.attention as attention
+  , a.addr1
+  , a.addr2
+  , a.addr3
+  , a.addressee
+  , a.addrphone
+  , a.addrtext
+  , a.attention
   , a.city
   , a.state
   , a.country
   , a.zip
   , a.override
-  , a.dropdownstate as state_drop_down
+  , a.dropdownstate
   from
     netsuite.cashsaleshippingaddress a
   where
     _fivetran_deleted = false
   union all
   select
-    a.nkey as order_address_id_ns
+    a.nkey
   , 'cashrefund' as record_type
-  , a.addr1 as address_1
-  , a.addr2 as address_2
-  , a.addr3 as address_3
-  , a.addressee as customer_name
-  , a.addrphone as phone_number
-  , a.addrtext as address_text
-  , a.attention as attention
+  , a.addr1
+  , a.addr2
+  , a.addr3
+  , a.addressee
+  , a.addrphone
+  , a.addrtext
+  , a.attention
   , a.city
   , a.state
   , a.country
   , a.zip
   , a.override
-  , a.dropdownstate as state_drop_down
+  , a.dropdownstate
   from
     netsuite.cashrefundshippingaddress a
   where
     _fivetran_deleted = false
   union all
   select
-    a.nkey as order_address_id_ns
+    a.nkey
   , 'invoice' as record_type
-  , a.addr1 as address_1
-  , a.addr2 as address_2
-  , a.addr3 as address_3
-  , a.addressee as customer_name
-  , a.addrphone as phone_number
-  , a.addrtext as address_text
-  , a.attention as attention
+  , a.addr1
+  , a.addr2
+  , a.addr3
+  , a.addressee
+  , a.addrphone
+  , a.addrtext
+  , a.attention
   , a.city
   , a.state
   , a.country
   , a.zip
   , a.override
-  , a.dropdownstate as state_drop_down
+  , a.dropdownstate
   from
     netsuite.invoiceshippingaddress a
   where
     _fivetran_deleted = false
   union all
   select
-    a.nkey as order_address_id_ns
+    a.nkey
   , 'itemfulfillment' as record_type
-  , a.addr1 as address_1
-  , a.addr2 as address_2
-  , a.addr3 as address_3
-  , a.addressee as customer_name
-  , a.addrphone as phone_number
-  , a.addrtext as address_text
-  , a.attention as attention
+  , a.addr1
+  , a.addr2
+  , a.addr3
+  , a.addressee
+  , a.addrphone
+  , a.addrtext
+  , a.attention
   , a.city
   , a.state
   , a.country
   , a.zip
   , a.override
-  , a.dropdownstate as state_drop_down
+  , a.dropdownstate
   from
     netsuite.itemfulfillmentshippingaddress a
   where
     _fivetran_deleted = false
 )
 select
-    concat(b.record_type,'_',b.order_address_id_ns) as order_address_id_edw
-  , b.order_address_id_ns
-  , b.record_type
-  , b.address_1
-  , b.address_2
-  , b.address_3
-  , b.customer_name
-  , b.phone_number
-  , NULLIF(REGEXP_REPLACE(b.phone_number, '^1|\\+1\\s?|\\(|\\)|-|\\s', ''), '') as normalized_phone_number
-  , b.address_text
+    concat(b.record_type,'_',b.nkey) as order_address_id_edw
+  , b.nkey as order_address_id_ns
+  , b.addr1 as address_1
+  , b.addr2 as address_2
+  , b.addr3 as address_3
+  , b.addressee as customer_name
+  , b.addrphone as phone_number
+  , NULLIF(REGEXP_REPLACE(b.addrphone, '^1|\\+1\\s?|\\(|\\)|-|\\s', ''), '') as normalized_phone_number
+  , b.addrtext as address_text
   , b.attention
   , b.city
   , b.state
@@ -122,7 +121,7 @@ select
   , b.country
   , b.zip
   , case when b.override = 'T' then true else false end as override_flag
-  , b.state_drop_down
+  , b.dropdownstate as state_drop_down
 from
   base b
 left join
