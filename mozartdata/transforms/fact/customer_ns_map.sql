fa2be5ab57684f741bb6f0db80d0a158b9bd943d
Josha Russell <russell.josha@gmail.com>
2025-03-07 16:59:50 -0800

    set cluster to 3 for remainder

--- mozartdata/transforms/fact/customer_ns_map.sql
+++ mozartdata/transforms/fact/customer_ns_map.sql
@@ -2,7 +2,7 @@ SELECT
   cust.customer_id_edw,
   nc.*,
   ct.tier as tier_2024,
-  cluster.cluster
+  case when cl.customer_id_ns is not null and cl.cluster is null then 3 else cl.cluster end as cluster
 FROM
   dim.CUSTOMER cust
 CROSS JOIN LATERAL FLATTEN(INPUT => cust.CUSTOMER_ID_NS) AS ns_ids
@@ -13,5 +13,5 @@ LEFT OUTER JOIN
     staging.customer_tier_snapshot_2024 ct
     on nc.customer_id_ns = ct.customer_id_ns
 LEFT OUTER JOIN
-    csvs.netsuite_b2b_customer_clusters cluster
-    on nc.customer_id_ns = cluster.customer_id_ns
+    csvs.netsuite_b2b_customer_clusters cl
+    on nc.customer_id_ns = cl.customer_id_ns
