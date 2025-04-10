70fafefe6e87121aa817b62f7b6e0033c3d8f8cc
KadenGoodr <131072641+KadenGoodr@users.noreply.github.com>
2025-04-10 11:59:28 -0700

    Cartesian join

--- mozartdata/transforms/fact/netsuite_bin_inventory.sql
+++ mozartdata/transforms/fact/netsuite_bin_inventory.sql
@@ -1,20 +1,57 @@
 -- CREATE OR REPLACE TABLE fact.netsuite_bin_inventory
 --             COPY GRANTS  as
+with
+    distinct_skus as (
+                         select distinct
+                             item
+                           , sku
+                           , location
+                         from
+                             staging.netsuite_bin_inventory binv
+                             left outer join dim.product    prod
+                                 on prod.item_id_ns = binv.item
+                     )
+  , days as (
+                         select
+                             date
+                         from
+                             dim.date
+                     )
+  , sku_date_grid as (
+                         select
+                             s.sku
+                           , s.item
+                           , s.location
+                           , c.date
+                         from
+                             distinct_skus   s
+                             cross join days c
+                     )
 select
-    binv.bin_id
-  , binv.binnumber as bin_name
-  , binv.zone_name
-  , binv.location as location_id
-  , binv.location_name
-  , binv.item
-  , prod.sku
+    grid.date
+  , grid.location
+  , grid.sku
   , prod.display_name
+  , binv.bin_id
+  , binv.binnumber               as bin_name
+  , binv.zone_name
+  , binv.location                as location_id
+  , binv.location_name
   , binv.snapshot_date_fivetran
-  , binv.quantityavailable as quantity_available
-  , binv.quantityonhand as quantity_on_hand
-  , binv.quantitypicked as quantity_picked
-  , binv.committedqtyperlocation committed_qty_per_location
+  , binv.quantityavailable       as quantity_available
+  , binv.quantityonhand          as quantity_on_hand
+  , binv.quantitypicked          as quantity_picked
+  , binv.committedqtyperlocation as committed_qty_per_location
 from
-    staging.netsuite_bin_inventory binv
-    left outer join dim.product    prod
-        on prod.item_id_ns = binv.item
+    sku_date_grid                      grid
+    left outer join
+        staging.netsuite_bin_inventory binv
+            on binv.snapshot_date_fivetran = grid.date and binv.item = grid.item and binv.location = grid.location
+    left outer join dim.product        prod
+        on prod.item_id_ns = grid.item
+where
+      date = '2025-04-02'
+  and grid.item = 105
+
+
+--date >= '2025-04-01'--This is the start of all the relevant data unless we backfill
