50c218debe938e21141c0b2e66ceca6bb05a34ea
Josha Russell <russell.josha@gmail.com>
2025-04-16 06:29:20 -0700

    add root

--- mozartdata/transforms/fact/netsuite_inventory_assignment.sql
+++ mozartdata/transforms/fact/netsuite_inventory_assignment.sql
@@ -1,3 +1,18 @@
+/*
+Purpose: This table has the bin inventory detail information used for Transfer Orders.
+
+Base table: CTE root_table is used to get root table reference for scheduling in mozart.
+If no longer a base table, then remove CTE root_table.
+*/
+
+with
+  root_table as (
+    select
+        *
+    from
+        mozart.pipeline_root_table
+    )
+
 SELECT
   ia.id as inventory_assignment_id_ns
 , abs(ia.quantity) as quantity
